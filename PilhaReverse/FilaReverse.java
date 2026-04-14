b5c1248f9d54ffa818f1c8dff5b02c90b9fceb80
Elton Bruno <eltonbruno07@gmail.com>
2026-04-14 09:02:48 -0300

    Update FilaReverse.java

--- PilhaReverse/FilaReverse.java
+++ PilhaReverse/FilaReverse.java
@@ -1,14 +1,57 @@
 public class FilaReverse {
     private int inicio, fim, tamanho, capacidade;
+    private int[] FilaArray;
 
     public FilaReverse(int cap){
         this.inicio = 0;
-        this.fim = 0;
+        this.fim =0;
         this.tamanho= 0;
         this.capacidade = cap;
+        this.FilaArray = new int[capacidade];
+    }
+
+    public void increaseCapacity(){
+        int capacidadeAntiga = this.capacidade;
+        this.capacidade *= 2;
+        Object[] ArrayGeralAumentado = new Object[this.capacidade];
+
+
+
+    }
+
+    public void reduct(){
+
+    }
+
+    public void reverse(){
+
+    }
+
+    public void enqueue(int value){
+        if(tamanho==capacidade){
+            increaseCapacity();
+        }
+        if(fim+1 == capacidade){
+            fim=0;
+        }
+
+        FilaArray[fim] = value;
+        tamanho++;
+    }
+
+    public int dequeue(){
+        if(fim-1 < 0){
+            fim=capacidade;
+        }
+        return fim;
+    }
+
+    public int size(){
+        return tamanho;
     }
 
     public boolean isEmpty(){
         return inicio==fim;
     }
+
 }
